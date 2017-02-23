0d6f6a924987575e51861f90c115cc699003bf3c
Christian Durán <asistentedeprincipios2@gmail.com>
2017-02-23 17:44:23 -0600

    Adds documentation and simplified code

--- stlparser.d
+++ stlparser.d
@@ -3,12 +3,14 @@ module stlparser;
 auto ref parseSTL (in string filename) {
     import std.stdio : File;
     auto file = File (filename);
+    import std.algorithm : map, find;
+    import std.string : strip;
     auto lines = file
-        .byLineCopy;
+        .byLineCopy
+        .map!strip;
     import std.regex : ctRegex, matchFirst;
     auto solidRegex  = ctRegex!`^solid\s+(?:\"(?:\\.|[^\\"])*\")?$`;
     import std.exception : enforce;
-    import std.string : strip;
     enforce (!lines.empty && lines.front.strip.matchFirst (solidRegex)
     /**/ , `ASCII stl file doesn't start with 'solid '`);
     lines.popFront ();
@@ -16,34 +18,32 @@ auto ref parseSTL (in string filename) {
     Appender!(float []) facets   = [];
     Appender!(float []) vertices = [];
     foreach (line; lines) {
-        // Splits a string with floats separated by spaces to the floats.
+        /// Splits a string with floats separated by spaces to the floats.
         static auto ref toFloats (in string toParse) {
-            import std.algorithm : map;
             import std.conv      : to;
-            return toParse.split(' ').map!(n => n.to!float);
+            return toParse.split(' ').map!(to!float);
         }
-        line = line.strip;
         enum floatsRegex = `((?:[+-]?(?:\d*[.])?\d+(?:e[+-]?\d+)?\s*){3})`;
-        import std.algorithm : find;
-        [
+
+        [   // Adds facets and vertices to their respective Appender.
             () { return line.tryMatch!(`^facet\s+normal\s+` ~ floatsRegex ~ `$`)
             /**/ (facet  => facets   ~= toFloats (facet)); },
             () { return line.tryMatch!(`^vertex\s+` ~ floatsRegex ~ `$`)
             /**/ (vertex => vertices ~= toFloats (vertex)); }
-        ].find!`a()`(true);
+        ].find!`a()`(true); // Tries each function until one returns true.
     }
     auto returnVertices = vertices.data;
     auto returnFacets   = facets.data;
     import std.conv : text;
-    enforce (returnVertices.length == returnFacets.length * 3
+    enforce ( returnVertices.length == returnFacets.length * 3
     /**/ , text (`There aren't 3 times as much vertices as facets: `
-    /**/ , returnVertices.length, ` `, returnFacets.length)
-    );
+    /**/ , returnVertices.length, ` `, returnFacets.length) );
     import std.typecons : tuple;
     return tuple (returnVertices, returnFacets);
 }
 
-
+/// If the line matches the regexExpression, executes action and returns true,
+/// else returns false.
 private bool tryMatch 
 /**/ (string regexExpression) (in string line, void delegate (string) action) {
     import std.regex : ctRegex, matchFirst;
