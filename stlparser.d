b2a7b2f131353378c8d1461d779702e9b80f77e3
Christian Durán <asistentedeprincipios2@gmail.com>
2017-02-23 17:27:08 -0600

    Simplifies algorithm

--- stlparser.d
+++ stlparser.d
@@ -16,12 +16,6 @@ auto ref parseSTL (in string filename) {
     Appender!(float []) facets   = [];
     Appender!(float []) vertices = [];
     foreach (line; lines) {
-        // Tries each action until one returns true.
-        static void matchSwitch (bool delegate () [] actions) {
-            foreach (action; actions) {
-                if (action()) return;
-            }
-        }
         // Splits a string with floats separated by spaces to the floats.
         static auto ref toFloats (in string toParse) {
             import std.algorithm : map;
@@ -30,27 +24,28 @@ auto ref parseSTL (in string filename) {
         }
         line = line.strip;
         enum floatsRegex = `((?:[+-]?(?:\d*[.])?\d+(?:e[+-]?\d+)?\s*){3})`;
-        matchSwitch ([
+        import std.algorithm : find;
+        [
             () { return line.tryMatch!(`^facet\s+normal\s+` ~ floatsRegex ~ `$`)
             /**/ (facet  => facets   ~= toFloats (facet)); },
             () { return line.tryMatch!(`^vertex\s+` ~ floatsRegex ~ `$`)
-            /**/ (vertex => vertices ~= toFloats (vertex)); },
-        ]);
+            /**/ (vertex => vertices ~= toFloats (vertex)); }
+        ].find!`a()`(true);
     }
     auto returnVertices = vertices.data;
     auto returnFacets   = facets.data;
     import std.conv : text;
     enforce (returnVertices.length == returnFacets.length * 3
     /**/ , text (`There aren't 3 times as much vertices as facets: `
-    /**/ , returnVertices, ` `, returnFacets.length)
+    /**/ , returnVertices.length, ` `, returnFacets.length)
     );
     import std.typecons : tuple;
     return tuple (returnVertices, returnFacets);
 }
 
 
-private bool tryMatch (string regexExpression)(in string line
-/**/ , void delegate (string) action) {
+private bool tryMatch 
+/**/ (string regexExpression) (in string line, void delegate (string) action) {
     import std.regex : ctRegex, matchFirst;
     auto matchedLine = line.matchFirst (ctRegex!regexExpression);
     if (!matchedLine.empty) { // Matches.
